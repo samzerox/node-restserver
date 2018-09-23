868222f73ad8bdb7bc18ccea91fbf6fe4bcf562e
samzerox <sam.zerox88@gmail.com>
2018-09-23 15:20:54 -0500

    Se agrego mongoose-unique-validator

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
 const bodyParser = require('body-parser');
@@ -12,35 +14,13 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario');
-});
-app.post('/usuario', function(req, res) {
-    let body = req.body;
+app.use(require('./routes/usuario'));
 
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
+mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
+    if (err) throw err;
 
-    res.json({
-        persona: body
-    });
-});
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario');
+    console.log('Base de datos ONLINE');
+
 });
 
 app.listen(process.env.PORT, () => {
