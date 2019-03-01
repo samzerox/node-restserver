898423e510fe3f322e5f76c11d14aae5dc503c9b
samzerox <sam.zerox88@gmail.com>
2019-02-28 18:10:29 -0600

    Subida de archivos

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -21,8 +21,6 @@ let verificaToken = (req, res, next) => {
 
         req.usuario = decoded.usuario
         next();
-
-
     });
 
 };
@@ -45,13 +43,36 @@ let verificaAdmin_Role = (req, res, next) => {
             }
         });
     }
-
-
-
 };
+
+
+// =======================
+// Verificar Token para imagen
+// =======================
+
+let verificaTokenImg = (req, res, next) => {
+    let token = req.query.token;
+
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err: {
+                    message: 'Token no válido'
+                }
+            });
+        }
+
+        req.usuario = decoded.usuario
+        next();
+    });
+}
+
+
 
 
 module.exports = {
     verificaToken,
-    verificaAdmin_Role
+    verificaAdmin_Role,
+    verificaTokenImg
 }
